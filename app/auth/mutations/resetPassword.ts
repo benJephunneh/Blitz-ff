9beb8fee7b9ed36339f4211d22aa3e1b761c351c
benJephunneh <trevithick@hotmail.com>
2022-08-21 16:06:29 -0400

    Get init auth working with username

--- app/auth/mutations/resetPassword.ts
+++ app/auth/mutations/resetPassword.ts
@@ -42,7 +42,7 @@ export default resolver.pipe(resolver.zod(ResetPassword), async ({ password, tok
   await db.session.deleteMany({ where: { userId: user.id } })
 
   // 7. Now log the user in with the new credentials
-  await login({ email: user.email, password }, ctx)
+  await login({ username: user.username, password }, ctx)
 
   return true
 })
