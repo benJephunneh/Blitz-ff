e2ae081ea2dd44cc593e55ed936b7eadfff27d26
benJephunneh <trevithick@hotmail.com>
2022-08-22 12:46:06 -0400

    Init Chakra-UI

--- app/core/components/LabeledTextField.tsx
+++ app/core/components/LabeledTextField.tsx
@@ -1,7 +1,8 @@
+import { FormControl, FormLabel, Input } from "@chakra-ui/react"
 import { forwardRef, ComponentPropsWithoutRef, PropsWithoutRef } from "react"
 import { useField, UseFieldConfig } from "react-final-form"
 
-export interface LabeledTextFieldProps extends PropsWithoutRef<JSX.IntrinsicElements["input"]> {
+export interface LabeledTextFieldProps extends ComponentPropsWithoutRef<typeof Input> {
   /** Field name. */
   name: string
   /** Field label. */
@@ -30,35 +31,16 @@ export const LabeledTextField = forwardRef<HTMLInputElement, LabeledTextFieldPro
     const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
 
     return (
-      <div {...outerProps}>
-        <label {...labelProps}>
-          {label}
-          <input {...input} disabled={submitting} {...props} ref={ref} />
-        </label>
+      <FormControl {...outerProps}>
+        <FormLabel {...labelProps}>{label}</FormLabel>
+        <Input {...input} disabled={submitting} {...props} ref={ref} />
 
         {touched && normalizedError && (
           <div role="alert" style={{ color: "red" }}>
             {normalizedError}
           </div>
         )}
-
-        <style jsx>{`
-          label {
-            display: flex;
-            flex-direction: column;
-            align-items: start;
-            font-size: 1rem;
-          }
-          input {
-            font-size: 1rem;
-            padding: 0.25rem 0.5rem;
-            border-radius: 3px;
-            border: 1px solid purple;
-            appearance: none;
-            margin-top: 0.5rem;
-          }
-        `}</style>
-      </div>
+      </FormControl>
     )
   }
 )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import React from "react"
 import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
 import { AuthenticationError, AuthorizationError } from "blitz"
-import React from "react"
 import { withBlitz } from "app/blitz-client"
+import { ChakraProvider } from "@chakra-ui/react"
 
 function RootErrorFallback({ error }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
@@ -25,9 +26,11 @@ function RootErrorFallback({ error }: ErrorFallbackProps) {
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary FallbackComponent={RootErrorFallback}>
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <ChakraProvider>
+      <ErrorBoundary FallbackComponent={RootErrorFallback}>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </ChakraProvider>
   )
 }
 
