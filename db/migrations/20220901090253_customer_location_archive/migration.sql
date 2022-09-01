1388be3bafd0f3d845b9519e5c82f0d7248964ce
Caleb <trevithick@hotmail.com>
2022-09-01 05:03:34 -0400

    Prisma migrate for cust-loc archive

---
+++ db/migrations/20220901090253_customer_location_archive/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "CustomerArchive" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "LocationArchive" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "primary" BOOLEAN NOT NULL DEFAULT true,
+    "house" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL DEFAULT 'FL',
+    "zipcode" TEXT NOT NULL,
+    "block" TEXT,
+    "lot" TEXT,
+    "parcel" TEXT,
+    "customerId" INTEGER NOT NULL,
+    CONSTRAINT "LocationArchive_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "CustomerArchive" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
