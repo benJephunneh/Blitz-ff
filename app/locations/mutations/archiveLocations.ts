cbe7276665c5b2ea44cc9bb98652000779e15450
Caleb <trevithick@hotmail.com>
2022-09-01 10:19:56 -0400

    Slight work on archivng, nothing operational

--- app/locations/mutations/archiveLocations.ts
+++ app/locations/mutations/archiveLocations.ts
@@ -5,30 +5,37 @@ import { ArchiveLocation } from "../validations"
 
 interface ArchiveLocationInput extends Pick<Prisma.LocationFindManyArgs, "where"> {}
 
+// See discussion on batch operations:
+//  https://github.com/prisma/prisma/issues/4998
+
 export default resolver.pipe(
   resolver.authorize(["Admin", "Owner"]),
   async ({ where }: ArchiveLocationInput) => {
-    // Get customer to be archived:
-    const locations = await db.location.findMany({ where })
-    if (!locations) throw new NotFoundError(`Customer ${where} not found.`)
+    return await db.$transaction(async (prisma) => {
+      const locations = await prisma.location.findMany({ where })
+      // const archiveLocations = await prisma.locationArchive.c
 
-    await new Promise((resolve) => {
-      resolve(archiveJobsMutation)
-    }).then(async () => {
-      await new Promise((resolve) => {
-        resolve(archiveInvoicesMutation)
-      }).then(async () => {
-        await new Promise((resolve) => {
-          resolve(archiveEstimatesMutation)
-        })
-      })
-    })
+      // const [archiveJobsMutation] = useMutation(archiveJobs)
+      // const [archiveInvoicesMutation] = useMutation(archiveInvoices)
+      // const [archiveEstimatesMutation] = useMutation(archiveEstimates)
 
-    // Create archival copy:
-    await db.customer.create({
-      data: {
-        ...customer,
-      },
+      // Get customer to be archived:
+      if (!locations) throw new NotFoundError(`Customer ${where} not found.`)
+
+      // Archive the independent models, first:
+      // await new Promise((resolve) => {
+      //   resolve(archiveJobsMutation)
+      // }).then(async () => {
+      //   await new Promise((resolve) => {
+      //     resolve(archiveInvoicesMutation)
+      //   }).then(async () => {
+      //     await new Promise((resolve) => {
+      //       resolve(archiveEstimatesMutation)
+      //     })
+      //   })
+      // })
+
+      // Create archival copy:
     })
   }
 )
