8e61f0240ce591c634ad444d4fb288e102be7540
Caleb <trevithick@hotmail.com>
2022-10-14 21:21:54 -0400

    Add jquery, clndr, moment, and keep lodash.

---
+++ pages/clndr/index.tsx
@@ -0,0 +1,8 @@
+import { BlitzPage } from "@blitzjs/next"
+import { Container } from "@chakra-ui/react"
+
+const ClndrPage: BlitzPage = () => {
+  return <Container></Container>
+}
+
+export default ClndrPage
