b1875c074ba61e52e50aee9058f96fd058766f35
Caleb <cpmtrevithick@gmail.com>
2023-01-05 14:10:15 -0500

    Add locate mutations and query

---
+++ app/locates/validations.ts
@@ -0,0 +1,18 @@
+import stashContentSchema from "app/core/components/editor/schema/stashContentSchema"
+import { z } from "zod"
+
+const id = z.number()
+const title = z.string()
+const notes = z.string()
+const query = z.string()
+
+export const LocateSkeleton = z.object({
+  title,
+  notes,
+})
+
+export const FullLocate = LocateSkeleton.extend({ id })
+export const CreateLocate = LocateSkeleton
+export const UpdateLocate = LocateSkeleton.partial().extend({ id })
+export const DeleteLocate = z.object({ id })
+export const FindLocate = z.object({ query })
